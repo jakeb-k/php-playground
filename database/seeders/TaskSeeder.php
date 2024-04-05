272101d482fe394836f568db196801d0be4e1774
jakeb-k <jknow30@outlook.com>
2024-04-05 14:56:06 +1000

    seeder + controller init

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('tasks')->insert([
+            'task' => 'Go buy groceries',
+            'priority'=> 1, 
+            'due_date' => Carbon::now()->addDays(1), 
+        ]);
+        DB::table('tasks')->insert([
+            'task' => 'Clean your room',
+            'priority'=> 2, 
+            'due_date' => Carbon::now()->addDays(2), 
+        ]);
+        DB::table('tasks')->insert([
+            'task' => 'Watch new podcast',
+            'priority'=> 3, 
+            'due_date' => Carbon::now()->addDays(3), 
+        ]);
+        
+    }
+}
